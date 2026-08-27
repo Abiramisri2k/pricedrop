7405a87c8d3344863e0df8468eb03a1968d2b93b
Abiramisri2k <abiramisri2k@gmail.com>
2026-08-27 11:25:08 +0530

    Email alerts

--- app/layout.js
+++ app/layout.js
@@ -2,16 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "Price Drop",
   description: "",
@@ -21,10 +11,9 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
-      <Toaster/>
+      <Toaster richColors/>
     </html>
   );
 }
